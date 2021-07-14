88df91688aa5a21e0112484b087a92683cc2efcc
sunny <chldbtjs74@gmail.com>
2021-07-14 10:44:35 +0900

    7/14 class1 commit 4

--- gis1/settings.py
+++ gis1/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accountapp'
 ]
 
@@ -150,4 +151,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
-LOGOUT_REDIRECT_URL = reverse_lazy('accoutapp:login')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
